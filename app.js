451ffb03a3d039b2373bb98da20fe58b227f5718
Carter <chalfmann9547@myhta.org>
2018-03-15 14:28:05 -1000

    Rough Draft Js

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
-window.onload = function(){
-    var dirt=document.getElementsByClassName("dirt");
-    var sound=document.getElementById("sound");
-    var grid=document.getElementById("dirt-box");
-console.log(dirt.innerHtml);
-    dirt.addEventListener("click", function(){
-       if (dirt.innerHtml=undefined) {
-           console.log("nothing in here")
-       } else {
-           sound.play();
-           
-       }
-    })
- 
-    function setMole(){
-var mole=document.getElementById("mole");
-var randomnum= math.random(0, dirt.length);
-console.log(randomnum);
+window.onload = function () {
+    var dirt = document.getElementsByClassName("dirt");
+    var sound = document.getElementById("sound");
+    var grid = document.getElementById("dirt-box");
+
+
+    for (i = 0; i < dirt.length; i++) {
+        dirt[i].addEventListener("click", function () {
+            if(dirt.innerHTML="mole"){
+dirt.innerHTML="";
+sound.play();
+            }else{
+            
+                console.log("Nothing here, boi");
+               
+            }
+
+
+        })
+
+
+    };
+
+
+
+    
+
+    function setMole() {
+        var mole = document.createElement("div");
+mole.id="mole";
+       
+var randomnum = Math.floor(Math.random(0, dirt.length))
+        
+
+        if(moles[randomnumber].innerHTML = "") {
+            dirt.innerHTML = "mole";
+        } else {
+            console.log("Too many moles");
+        }
 
     }
+
+
+
+setInterval(setMole(), 3000);
+
+
 }
